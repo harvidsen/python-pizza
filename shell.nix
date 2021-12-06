7bd0a96fe18f95d83eb12734da81654e06a52ce1
harvidsen <hakon.arvidsen@bkk.no>
2021-12-06 17:46:52 +0100

    Add nix-shell definition

---
+++ shell.nix
@@ -0,0 +1,26 @@
+{   
+  pkgs ? import (fetchTarball 
+    https://github.com/NixOS/nixpkgs/archive/refs/tags/21.11.tar.gz
+  ) {}
+}:
+with pkgs.python39Packages;
+
+pkgs.mkShell {
+  buildInputs = [
+    pandas numpy requests scikit-learn azure-storage-blob jupyter
+  ];
+  shellHook = ''
+    # Set environment variables
+    export BPEXT_DATASETS_SAS_URL="$HOME/secret/azure/bpext-datasets-sas-url"
+
+    # Fix vscode python settings
+    mkdir -p .vscode
+    cat > .vscode/settings.json <<EOF
+    {
+      "python.defaultInterpreterPath": "''$(which python)",
+      "python.analysis.extraPaths": [ "''${PYTHONPATH//:/'", "'}" ],
+    }
+    EOF
+    export PYTHONPATH="$PYTHONPATH:$PWD"
+  '';
+}
